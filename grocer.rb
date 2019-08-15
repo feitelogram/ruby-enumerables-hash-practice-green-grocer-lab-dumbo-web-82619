fbe818d719e6770ff284d0e4782a1218103f9b4a
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-15 13:46:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -57,7 +57,7 @@ def apply_clearance(cart)
 end
 
 def checkout(cart, coupons)
-  cart = consolidate_cart (cart)
+  cons_cart = consolidate_cart (cart)
   cart =apply_coupons(cart, coupons)
   bindi
   cart =apply_clearance (cart)
